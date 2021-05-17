ee46bb68bf057dc49c031b6952f4686874d78a52
magitex <avik.rswebtech@gmail.com>
2021-05-17 20:12:56 +0530

    jkjujyhujhtthikiiko9iuu

--- components/shared/headers/HeaderDefault.jsx
+++ components/shared/headers/HeaderDefault.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, {useEffect,useState} from 'react';
 import Logo from '~/components/elements/common/Logo';
 import SearchHeader from '~/components/shared/headers/modules/SearchHeader';
 import NavigationDefault from '~/components/shared/navigation/NavigationDefault';
 import HeaderActions from '~/components/shared/headers/modules/HeaderActions';
 import { stickyHeader } from '~/utilities/common-helpers';
 import MenuCategoriesDropdown from '~/components/shared/menus/MenuCategoriesDropdown';
-
+import Hyperlocation from '~/components/shared/headers/modules/Hyperlocation';
+const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
 const HeaderDefault = () => {
+    const [isOpen, setIsOpen] = useState(false);
     useEffect(() => {
         if (process.browser) {
             window.addEventListener('scroll', stickyHeader);
@@ -28,7 +32,20 @@ const HeaderDefault = () => {
                         <SearchHeader />
                     </div>
                     <div className="header__center">
-                        Pickup Location
+                         <input
+      type="button"
+      value="Click to Open pickup"
+      onClick={togglePopup}
+    />
+     {isOpen && <Popup
+      content={<>
+        <b>Design your Popup</b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>Test button</button>
+      </>}
+      handleClose={togglePopup}
+    />}
+    
                     </div>
                     <div className="header__right">
                         <HeaderActions />

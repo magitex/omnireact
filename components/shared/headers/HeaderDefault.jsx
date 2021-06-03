6e8eabde511203ef197f9f093dc4e21de4be05ae
magitex <avik.rswebtech@gmail.com>
2021-06-03 18:40:55 +0530

    make it better

--- components/shared/headers/HeaderDefault.jsx
+++ components/shared/headers/HeaderDefault.jsx
@@ -66,7 +66,7 @@ const HeaderDefault = () => {
                     </div>
                     <div className="header__center">
                      <a className="address_holder" onClick={togglePopup}>
-                     {delivery} Pickup,
+                     {delivery} Pickupllllll,
                      <br/>    
                      {fulladdress}
                         </a>
